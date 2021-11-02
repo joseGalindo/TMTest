be040201251d70ee3431b4e45c033f0712d0c762
Jose Cruz Galindo Martinez <jose.galindomtz@gmail.com>
2021-11-02 17:08:45 -0500

    Creating the MVVM

---
+++ TMChallenge/PresentationLayer/Feeds/FeedsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FeedsViewController.swift
+//  TMChallenge
+//
+//  Created by Jose Galindo Martinez on 02/11/21.
+//
+
+import UIKit
+
+class FeedsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TMChallenge/PresentationLayer/Feeds/FeedsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  TMChallenge
+//
+//  Created by Jose Galindo Martinez on 02/11/21.
+//
+
+import Foundation
+
+protocol FeedsViewModel: AnyObject {}
+
+class FeedsViewModelImplementation: FeedsViewModel {}
